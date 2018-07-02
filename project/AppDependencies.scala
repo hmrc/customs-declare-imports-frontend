8ed68be0785507d839ae9d08b53bcfaee10bb5f5
Christopher Townson <iteratoruk@gmail.com>
2018-07-02 18:10:44 +0100

    Remove dependency on hmrctest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,12 +13,11 @@ object AppDependencies {
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.10.2" % scope,
-
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope
   )
 
 }
--- test/controllers/HelloWorldControllerSpec.scala
+++ test/controllers/HelloWorldControllerSpec.scala
@@ -18,33 +18,28 @@ package controllers
 
 import config.AppConfig
 import play.api.http.Status
-import play.api.i18n.{DefaultLangs, DefaultMessagesApi}
+import play.api.i18n.MessagesApi
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.{Configuration, Environment}
-import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import uk.gov.hmrc.customs.test.ControllerSpec
 
-class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
-  val fakeRequest = FakeRequest("GET", "/")
+class HelloWorldControllerSpec extends ControllerSpec {
 
-  val env = Environment.simple()
-  val configuration = Configuration.load(env)
-
-  val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
-  val appConfig = new AppConfig(configuration, env)
-
-  val controller = new HelloWorld(messageApi, appConfig)
+  val req = FakeRequest("GET", "/")
+  val messages = app.injector.instanceOf[MessagesApi]
+  val config = app.injector.instanceOf[AppConfig]
+  val controller = new HelloWorld(messages, config)
 
   "GET /" should {
     "return 200" in {
-      val result = controller.helloWorld(fakeRequest)
-      status(result) shouldBe Status.OK
+      val result = call(controller.helloWorld, req)
+      status(result) must be (Status.OK)
     }
 
     "return HTML" in {
-      val result = controller.helloWorld(fakeRequest)
-      contentType(result) shouldBe Some("text/html")
-      charset(result) shouldBe Some("utf-8")
+      val result = call(controller.helloWorld, req)
+      contentType(result) must be (Some("text/html"))
+      charset(result) must be (Some("utf-8"))
     }
 
   }
