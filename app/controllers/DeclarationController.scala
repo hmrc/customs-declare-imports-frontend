a96b15f91251b048d87a159a6ffc71fed171cfb9
Christopher Townson <iteratoruk@gmail.com>
2018-07-16 15:52:03 +0100

    Initial cut of declaration form

--- app/controllers/DeclarationController.scala
+++ app/controllers/DeclarationController.scala
@@ -17,8 +17,11 @@
 package controllers
 
 import config.AppConfig
+import domain.declaration.{Declaration, MetaData}
 import domain.features.Feature
 import javax.inject.{Inject, Singleton}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
@@ -28,8 +31,44 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class DeclarationController @Inject()(actions: Actions, val messagesApi: MessagesApi)(implicit val appConfig: AppConfig, ec: ExecutionContext) extends FrontendController with I18nSupport {
 
+  val declarationForm: Form[DeclarationForm] = Form(
+    mapping(
+      "wcoDataModelVersionCode" -> optional(text),
+      "wcoTypeName" -> optional(text),
+      "responsibleCountryCode" -> optional(text),
+      "responsibleAgencyName" -> optional(text),
+      "agencyAssignedCustomizationCode" -> optional(text),
+      "agencyAssignedCustomizationVersionCode" -> optional(text)
+    )(DeclarationForm.apply)(DeclarationForm.unapply)
+  )
+
   def showDeclarationForm: Action[AnyContent] = (actions.switch(Feature.declaration) andThen actions.auth).async { implicit req =>
-    Future.successful(Ok(views.html.declaration_form()))
+    Future.successful(Ok(views.html.declaration_form(declarationForm)))
   }
+
+  def handleDeclarationForm: Action[AnyContent] = (actions.switch(Feature.declaration) andThen actions.auth).async { implicit req =>
+    Future.successful(Ok(views.html.declaration_acknowledgement()))
+  }
+
+}
+
+// At present, our form mirrors the declaration XML exactly. Later this may change. Therefore, it is probably useful
+// to retain a distinction between view model class and XML model class and map the former to the latter
+case class DeclarationForm(wcoDataModelVersionCode: Option[String] = None,
+                           wcoTypeName: Option[String] = None,
+                           responsibleCountryCode: Option[String] = None,
+                           responsibleAgencyName: Option[String] = None,
+                           agencyAssignedCustomizationCode: Option[String] = None,
+                           agencyAssignedCustomizationVersionCode: Option[String] = None) {
+
+  def toMetaData: MetaData = MetaData(
+    Declaration(),
+    wcoDataModelVersionCode,
+    wcoTypeName,
+    responsibleCountryCode,
+    responsibleAgencyName,
+    agencyAssignedCustomizationCode,
+    agencyAssignedCustomizationVersionCode
+  )
 
 }
--- test/controllers/DeclarationControllerSpec.scala
+++ test/controllers/DeclarationControllerSpec.scala
@@ -17,11 +17,13 @@
 package controllers
 
 import domain.features.{Feature, FeatureStatus}
+import play.api.test.Helpers._
 import uk.gov.hmrc.customs.test.{AuthenticationBehaviours, CustomsPlaySpec, FeatureSwitchBehaviours}
 
 class DeclarationControllerSpec extends CustomsPlaySpec with AuthenticationBehaviours with FeatureSwitchBehaviours {
 
   val method = "GET"
+  val handleMethod = "POST"
   val uri = uriWithContextPath("/declaration")
 
   s"$method $uri" should {
@@ -48,6 +50,129 @@ class DeclarationControllerSpec extends CustomsPlaySpec with AuthenticationBehav
       signedInScenario(signedInUser) {
         userRequestScenario(method, uri, signedInUser) { wasNotFound }
       }
+    }
+
+    "include a text input for WCO data model version code" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "wcoDataModelVersionCode")
+        }
+      }
+    }
+
+    "include a text input for WCO type name" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "wcoTypeName")
+        }
+      }
+    }
+
+    "include a text input for Responsible Country Code" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "responsibleCountryCode")
+        }
+      }
+    }
+
+    "include a text input for Responsible Agency Name" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "responsibleAgencyName")
+        }
+      }
+    }
+
+    "include a text input for Agency Assigned Customization Code" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "agencyAssignedCustomizationCode")
+        }
+      }
+    }
+
+    "include a text input for Agency Assigned Customization Version Code" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario() {
+        userRequestScenario(method, uri) { resp =>
+          includesHtmlInput(resp, "text", "agencyAssignedCustomizationVersionCode")
+        }
+      }
+    }
+
+  }
+
+  s"$handleMethod $uri" should {
+
+    "return 200" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario(signedInUser) {
+        userRequestScenario(handleMethod, uri, signedInUser) { wasOk }
+      }
+    }
+
+    "return HTML" in featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      signedInScenario(signedInUser) {
+        userRequestScenario(handleMethod, uri, signedInUser) { wasHtml }
+      }
+    }
+
+    // FIXME why does this not pass?
+    "require authentication" ignore featureScenario(Feature.declaration, FeatureStatus.enabled) {
+      notSignedInScenario() {
+        accessDeniedRequestScenarioTest(handleMethod, uri)
+      }
+    }
+
+    "be behind a feature switch" in featureScenario(Feature.declaration, FeatureStatus.disabled) {
+      signedInScenario(signedInUser) {
+        userRequestScenario(handleMethod, uri, signedInUser) { wasNotFound }
+      }
+    }
+
+  }
+
+  "declaration form mapping to domain object" should {
+
+    "map WCO data model version code" in {
+      val code = randomString(8)
+      DeclarationForm(
+        wcoDataModelVersionCode = Some(code)
+      ).toMetaData.wcoDataModelVersionCode.get must be (code)
+    }
+
+    "map WCO type name" in {
+      val name = randomString(8)
+      DeclarationForm(
+        wcoTypeName = Some(name)
+      ).toMetaData.wcoTypeName.get must be (name)
+    }
+
+    "map responsible country code" in {
+      val code = randomString(2)
+      DeclarationForm(
+        responsibleCountryCode = Some(code)
+      ).toMetaData.responsibleCountryCode.get must be (code)
+    }
+
+    "map responsible agency name" in {
+      val name = randomString(16)
+      DeclarationForm(
+        responsibleAgencyName = Some(name)
+      ).toMetaData.responsibleAgencyName.get must be (name)
+    }
+
+    "map agency assigned customization code" in {
+      val code = randomString(8)
+      DeclarationForm(
+        agencyAssignedCustomizationCode = Some(code)
+      ).toMetaData.agencyAssignedCustomizationCode.get must be (code)
+    }
+
+    "map agency assigned customisation version code" in {
+      val code = randomString(8)
+      DeclarationForm(
+        agencyAssignedCustomizationVersionCode = Some(code)
+      ).toMetaData.agencyAssignedCustomizationVersionCode.get must be (code)
     }
 
   }
