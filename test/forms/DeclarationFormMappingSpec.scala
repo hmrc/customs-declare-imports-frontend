59ede12837b56e1885ce683bb6f5f782bedfea15
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2019-01-22 18:01:40 +0000

    WIP | added validation for missing items in form

--- test/forms/DeclarationFormMappingSpec.scala
+++ test/forms/DeclarationFormMappingSpec.scala
@@ -149,37 +149,41 @@ class DeclarationFormMappingSpec extends WordSpec
           )
         }
       }
+    }
 
-      "fail with invalid id" when {
+    "fail to bind" when {
 
-        "id length is greater than 17" in {
+      "id length is greater than 17" in {
 
-          forAll(arbitrary[AuthorisationHolder], minStringLength(18)) { (authorisationHolder, invalidId) =>
+        forAll(arbitrary[AuthorisationHolder], minStringLength(18)) { (authorisationHolder, invalidId) =>
 
-            Form(authorisationHolderMapping).fillAndValidate(authorisationHolder.copy(id = Some(invalidId))).fold(
-              error => error.error("id") must haveMessage("ID should be less than or equal to 17 characters"),
-              _     => fail("Should not succeed")
-            )
-          }
+          Form(authorisationHolderMapping).fillAndValidate(authorisationHolder.copy(id = Some(invalidId))).fold(
+            error => error.error("id") must haveMessage("ID should be less than or equal to 17 characters"),
+            _     => fail("Should not succeed")
+          )
         }
       }
 
-      "fail with invalid category code" when {
+      "category code length is greater than 4" in {
 
-        "category code length is greater than 4" in {
+        forAll(arbitrary[AuthorisationHolder], minStringLength(5)) { (authorisationHolder, invalidCategoryCode) =>
 
-          forAll(arbitrary[AuthorisationHolder], minStringLength(5)) { (authorisationHolder, invalidCategoryCode) =>
-
-            Form(authorisationHolderMapping).fillAndValidate(authorisationHolder.copy(categoryCode = Some(invalidCategoryCode))).fold(
-              error => error.error("categoryCode") must haveMessage("Category Code should be less than or equal to 4 characters"),
-              _     => fail("Should not succeed")
-            )
-          }
+          Form(authorisationHolderMapping).fillAndValidate(authorisationHolder.copy(categoryCode = Some(invalidCategoryCode))).fold(
+            error => error.error("categoryCode") must haveMessage("Category Code should be less than or equal to 4 characters"),
+            _     => fail("Should not succeed")
+          )
         }
+      }
+
+      "both id and category code are missing" in {
+
+        Form(authorisationHolderMapping).bind(Map[String, String]()).fold(
+          error => error must haveErrorMessage("You must provide an ID or category code"),
+          _     => fail("Should not succeed")
+        )
       }
     }
   }
-
 
   "alphaNum regular expression" should {
 
