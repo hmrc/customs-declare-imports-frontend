0ab074b2f20e84533e4b1a0fed356f519da66f16
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2019-02-22 15:08:24 +0000

    CDSID-654 | added view for failure page

---
+++ test/views/SubmitFailureSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import controllers.routes
+import play.twirl.api.Html
+import views.behaviours.ViewBehaviours
+import views.html.submit_failure
+
+class SubmitFailureSpec extends ViewBehaviours {
+
+  val view: () => Html = () => submit_failure()(fakeRequest, messages, appConfig)
+  val messagePrefix = "submitFailure"
+
+  "submit_failure view" should {
+
+    behave like normalPage(view, messagePrefix, "intro")
+    behave like pageWithBackLink(view)
+
+    "have link to re submit" in {
+
+      val doc = asDocument(view())
+      assertContainsLink(doc, messages(s"$messagePrefix.link"), routes.SubmitController.onSubmit().url)
+    }
+  }
+}
