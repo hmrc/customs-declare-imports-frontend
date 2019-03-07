5805fcb1c113124ffaad174817002b3086ca8a3e
Gorkem Unsal <gorkem.unsal@accenture.com>
2019-03-07 11:35:10 +0000

    CDSID-624 controller and spec file

--- app/controllers/goodsitems/GoodsItemsDetailsController.scala
+++ app/controllers/goodsitems/GoodsItemsDetailsController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.goodsitems
 
 import config.AppConfig
@@ -10,13 +26,34 @@ import play.api.i18n.MessagesApi
 import play.api.mvc.{Action, AnyContent}
 import services.CustomsCacheService
 import services.cachekeys.CacheKey
-import uk.gov.hmrc.wco.dec.Commodity
-import views.html.goodsitems.goods_items_commodity_details
+import uk.gov.hmrc.wco.dec.{Commodity, GovernmentAgencyGoodsItem}
+import views.html.goodsitems.goods_items_details
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class GoodsItemsDetailsController @Inject()(actions: Actions, cacheService: CustomsCacheService)
                                            (implicit appConfig: AppConfig, override val messagesApi: MessagesApi, ec: ExecutionContext)
-  extends CustomsController  {
+  extends CustomsController {
 
+  val form = Form(goodsItemDetailsMapping)
+
+  def onPageLoad: Action[AnyContent] = (actions.auth andThen actions.eori andThen actions.goodsItem) { implicit req =>
+
+    val popForm = form.fillAndValidate(req.goodsItem)
+    Ok(goods_items_details(popForm))
+  }
+
+  def onSubmit: Action[AnyContent] = (actions.auth andThen actions.eori andThen actions.goodsItem).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[GovernmentAgencyGoodsItem]) =>
+          Future.successful(BadRequest(goods_items_details(formWithErrors))),
+        goodsItem => {
+          println("controller :::::::::::::::::")
+          println(goodsItem)
+          cacheService.insert(request.eori, CacheKey.goodsItem, goodsItem).map { _ =>
+            Redirect(controllers.routes.GovernmentAgencyGoodsItemsController.showGoodsItemPage())
+          }
+        })
+  }
 }
--- test/controllers/goodsitems/GoodsItemsDetailsControllerSpec.scala
+++ test/controllers/goodsitems/GoodsItemsDetailsControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.goodsitems
 
 import controllers.FakeActions
@@ -16,6 +32,7 @@ import play.api.test.Helpers._
 import services.cachekeys.CacheKey
 import uk.gov.hmrc.customs.test.behaviours.{CustomsSpec, EndpointBehaviours}
 import uk.gov.hmrc.wco.dec.{Commodity, DutyTaxFee, GovernmentAgencyGoodsItem}
+import views.html.goodsitems.goods_items_details
 
 class GoodsItemsDetailsControllerSpec extends CustomsSpec
   with PropertyChecks
@@ -23,4 +40,133 @@ class GoodsItemsDetailsControllerSpec extends CustomsSpec
   with OptionValues
   with EndpointBehaviours  {
 
+  val form = Form(goodsItemDetailsMapping)
+
+  def view(form: Form[_]): String =
+    goods_items_details(form)(fakeRequest, messages, appConfig).body
+
+  def controller(user: Option[SignedInUser], item: Option[GovernmentAgencyGoodsItem]) =
+    new GoodsItemsDetailsController(new FakeActions(user, item), mockCustomsCacheService)
+
+  val uri = "/submit-declaration-goods/goods-item-details"
+
+  "onPageLoad" should {
+
+    behave like badRequestEndpoint(uri)
+    behave like authenticatedEndpoint(uri)
+
+    "return OK" when {
+
+      "user is signed in" in {
+
+        forAll { (signedInUser: SignedInUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+          val result = controller(Some(signedInUser), Some(goodsItem)).onPageLoad()(fakeRequest)
+
+          status(result) mustBe OK
+        }
+      }
+    }
+
+    "return Unauthorized" when {
+
+      "user doesn't have an eori" in {
+
+        forAll { (user: UnauthenticatedUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+          val result = controller(Some(user.user), Some(goodsItem)).onPageLoad(fakeRequest)
+
+          status(result) mustBe UNAUTHORIZED
+        }
+      }
+    }
+
+    "load data from cache" in {
+
+      forAll { (signedInUser: SignedInUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+        val result = controller(Some(signedInUser), Some(goodsItem)).onPageLoad()(fakeRequest)
+        val popForm = form.fillAndValidate(goodsItem)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe view(popForm)
+      }
+    }
+  }
+
+  "onSubmit" should {
+
+    behave like badRequestEndpoint(uri, POST)
+    behave like authenticatedEndpoint(uri, POST)
+
+    "return OK" when {
+
+      "user submits valid data" in {
+
+        forAll { (user: SignedInUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+          val request = fakeRequest.withFormUrlEncodedBody(asFormParams(goodsItem): _*)
+          val result = controller(Some(user), Some(goodsItem)).onSubmit(request)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.GovernmentAgencyGoodsItemsController.showGoodsItemPage().url)
+        }
+      }
+    }
+
+    "return UNAUTHORIZED" when {
+
+      "user does not have an eori" in {
+
+        forAll { (user: UnauthenticatedUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+          val result = controller(Some(user.user), Some(goodsItem)).onSubmit(fakeRequest)
+
+          status(result) mustBe UNAUTHORIZED
+        }
+      }
+    }
+
+    "return BAD_REQUEST" when {
+
+      "user submits invalid data" in {
+
+        val badData =
+          for {
+            goodsItem   <- arbitrary[GovernmentAgencyGoodsItem]
+            sequenceNum <- intGreaterThan(999)
+          } yield goodsItem.copy(sequenceNumeric = sequenceNum)
+
+        forAll(arbitrary[SignedInUser], badData, arbitrary[GovernmentAgencyGoodsItem]) {
+          case (user, formData, goodsItem) =>
+            val request = fakeRequest.withFormUrlEncodedBody(asFormParams(formData): _*)
+            val badForm = form.fillAndValidate(formData)
+            val result = controller(Some(user), Some(goodsItem)).onSubmit(request)
+
+            status(result) mustBe BAD_REQUEST
+            contentAsString(result) mustBe view(badForm)
+        }
+      }
+    }
+
+    "saves data in cache" when {
+
+      "valid data is provided" in {
+
+        forAll { (user: SignedInUser, goodsItem: GovernmentAgencyGoodsItem) =>
+
+          withCleanCache(EORI(user.eori.value), CacheKey.goodsItem, Some(goodsItem)) {
+            val request = fakeRequest.withFormUrlEncodedBody(asFormParams(goodsItem): _*)
+            await(controller(Some(user), Some(goodsItem)).onSubmit(request))
+
+            println("test::::::::::")
+            println(goodsItem)
+
+            verify(mockCustomsCacheService, atLeastOnce())
+              .insert(eqTo(EORI(user.eori.value)), eqTo(CacheKey.goodsItem), eqTo(goodsItem))(any(), any(), any())
+          }
+        }
+      }
+    }
+  }
 }
